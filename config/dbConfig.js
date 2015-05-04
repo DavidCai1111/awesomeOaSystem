ca5fd1f9990b5984f0048cf3a244c13a9081c327
DavidCai <376462191@qq.com>
2015-05-04 16:13:15 +0800

    first commit

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,9 +1,9 @@
 module.exports = {
-  cookieSecret: 'myblog',
-  db: 'blog',
-  host: '123.57.73.34',
-  dbUsername: 'dbAdmin',
-  dbPassword: 'dbAdmin',
-  collectionUsername: 'blogAdmin',
-  collecttionPassword: 'blogAdmin'
+  cookieSecret: 'your cookie secret',
+  db: 'your db',
+  host: 'yourhost',
+  dbUsername: 'your db username',
+  dbPassword: 'your db password',
+  collectionUsername: 'your collection username',
+  collecttionPassword: 'your collection password'
 };
